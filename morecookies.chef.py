2fdb2d7473364ccecf11d297dfc7cea0ac823d3a
Johanna <johanna.annau@gmail.com>
2021-01-12 16:32:44 +0100

    Bonus 8:making many cookies and pancakes!

---
+++ morecookies.chef.py
@@ -0,0 +1,72 @@
+from kitchen import Rosemary
+from kitchen.utensils import Pan, Plate, Bowl, BakingTray, Oven 
+from kitchen.ingredients import Butter, Egg, Salt, Flour, BakingPowder,Sugar, ChocolateChips
+import random
+
+#First: Preheat the oven
+oven=Oven.use()
+oven.preheat(degrees=175)
+
+#take a bowl
+bowl = Bowl.use(name='batter')
+
+#here we can specify the number of servings of 16 cookies we want (or let it be random!)
+number= random.randint (1,10)
+print(number)
+print ('times a serving of 16 cookies') 
+
+#Making a lot of dough
+for i in range(number):
+    #Mix sugar and Butter in little bits at a time
+    for i in range(4):
+        butter= Butter.take(grams= 50)
+        sugar = Sugar.take(grams=50)
+        bowl.add(sugar)
+        bowl.add(sugar)
+        bowl.mix()
+
+    #Once this is smooth add the eggs seperately
+    for i in range(2):
+        egg = Egg.take()
+        egg.crack()
+        bowl.add(egg)
+        bowl.mix()
+
+    #And for flavor:salt
+    bowl.add (Salt.take(' a pinch'))
+
+    #I'm mixing the dry ingredients seperate bowl
+    bowl2=Bowl.use(name='dry contents')
+    flour= Flour.take(grams=300)
+    chocolate = ChocolateChips.take (grams=200)
+    bakingpowder = BakingPowder.take ('some')
+    bowl2.add(flour)
+    bowl2.add(chocolate)
+    bowl2.add (bakingpowder)
+    bowl2.mix()
+
+    #Now add wet and dry together
+    for i in range (5):
+        bowl.add (bowl2.take('1/5'))
+        bowl.mix()
+
+#Let's try that batter!
+Rosemary.taste(bowl)
+
+#put generous scoopfuls of batter on the baking tray, making sure to keep distance!
+tray = BakingTray.use('Cookies')
+for i in range(number):
+    for i in range (16):
+        tray.add(bowl.take(f'1/{16*number}'))
+
+oven.add(tray)
+oven.bake(minutes=10)
+
+#Now serve!
+plate = Plate.use()
+cookies = tray.take()
+plate.add(cookies)
+
+#But wait for them to cool down!
+Rosemary.serve(plate)
+#Eet smakelijk!
---
+++ morepancakes.chef.py
@@ -0,0 +1,55 @@
+from kitchen import Rosemary
+from kitchen.utensils import Pan, Plate, Bowl
+from kitchen.ingredients import Butter, Egg, Salt, Flour, Milk
+import random
+
+
+#take a bowl
+bowl = Bowl.use(name='batter')
+#here we can specify the number of servings of 8 pancakes we want (or let it be random!)
+number= random.randint (1,10)
+print(number)
+print ('times a serving of 8 pancakes') 
+
+for i in range(number):
+    #whisk the eggs until frothy
+    for egg in Egg.take(2):
+        egg.crack()
+        bowl.add(egg)
+    bowl.mix()
+
+    #Add a dash of salt
+    bowl.add (Salt.take(' a dash'))
+
+    #Mix in the flour in 50g batches
+    for i in range(5):
+        flour= Flour.take(grams= 50)
+        bowl.add(flour)
+        bowl.mix
+
+    #Then add half the milk, mix, and then add the second half (does this actually do this?)
+    for i in range(2):
+        milk = Milk.take (ml=250)
+        bowl.add(milk)
+        bowl.mix
+Rosemary.taste(bowl)
+
+#Now it's time for baking!
+pan = Pan.use(name='pancake')
+for i in range(number):
+    for i in range (8):
+        pan.add(Butter.take('slice'))
+        pan.add(bowl.take(f'1/{8*number}'))
+        pan.cook(minutes=1)
+        #flip it once golden brown
+        pan.flip
+        pan.cook(minutes=1)
+
+
+#For the hungry ones: all the pancakes on one plate!
+plate = Plate.use()
+pancake = pan.take()
+plate.add(pancake)
+
+Rosemary.serve(pancake)
+#Bon Appetit!
